068f9d5e31910d81f7eeca1a7582dbc176e6f349
davidxiang101 <davidxiang101@gmail.com>
2023-10-25 12:42:38 -0400

    remove duplicate links

--- scrape/finder.py
+++ scrape/finder.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
 url = "https://medium.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-links = []
+links = set()
 article_pattern = re.compile(r"https://medium.com/@[^/]+/[^/]+")
 
 for a_tag in soup.find_all("a", {"rel": "noopener follow"}):
@@ -14,6 +15,15 @@ for a_tag in soup.find_all("a", {"rel": "noopener follow"}):
     if href.startswith("/@"):
         full_url = f"https://medium.com{href}"
         if article_pattern.match(full_url):
-            links.append(full_url)
+            links.add(full_url)
 
 print(links)
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# Update urls in the config dictionary
+config["urls"] = list(links)
+
+# Write updated config back to config.json
+with open("config.json", "w") as f:
+    json.dump(config, f, indent=4)
